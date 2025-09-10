be486a2933e7204c1aced8e3abba1c2ea0d56559
Emran-em <mdwaseem3944@gmail.com>
2025-09-10 17:40:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,3 @@ resource "local_file" "my_work" {
   filename = var.filename
   content  = var.content
 }
-
-resource "random_pet" "my_work" {
-  prefix    = var.prefix
-  separator = "."
-  length    = var.length
-}
